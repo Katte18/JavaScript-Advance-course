8c4e060c7be8091bf674b5859b94f1db82eeb1db
Kateryna <buleett@gmail.com>
2022-02-23 12:02:48 +0200

    add taks02

---
+++ 02task/main.js
@@ -0,0 +1,89 @@
+/* get integer N number from a user */
+
+let nNum;
+    do {
+        nNum = prompt("Please enter an integer number N:");
+   
+    } while (!isFinite(nNum) || nNum % 1 !== 0);
+
+    console.log("Integer N number:", nNum);
+
+/* get integer N number from a user */ 
+
+let mNum;
+    do {
+        mNum = prompt("Please enter an integer number M:");
+    
+    } while (!isFinite(mNum) || mNum % 1 !== 0);
+
+    console.log("Integer M number:", mNum);
+
+/* boolean parameter for omit even numbers */
+
+let evenNum = confirm ("Should omit even numbers?")
+console.log(evenNum);
+ 
+/* loop of N and M numbers */
+
+let evenSum = 0;
+for (let i = +nNum; i <= +mNum; i++) {
+    if (evenNum &&  i%2  === 0)  continue;
+    evenSum += i; 
+}
+console.log(evenSum);
+document.writeln("Output of sum of numbers: ", evenSum);
+
+/* loop of N and M numbers with add output */
+//let evenSum = 0;
+//let oddSum = 0;
+//for (i = +nNum; i <= +mNum; i++) {
+    //if (evenNum &&  i%2  === 0) {
+        //evenSum +=i;
+       // console.log(evenSum);
+    //}else{
+        //oddSum +=i;
+       // console.log(oddSum);
+  //  }
+//}
+  //  console.log(evenSum, oddSum);
+
+
+
+/* practice of rule check */
+
+  //  let a = prompt("Please input number:");
+
+  //      if (isFinite(a) && a % 1 === 0) {
+     //       console.log("number and integer", a);
+     //   }else if (isNaN(a)){
+     //       console.log("not a number");
+     //   }else if (a % 1 !== 0) {
+     //       console.log("not integer");
+     //   }else {
+     //       console.log("no data");
+     //   }
+
+ /* practice of switch */
+
+   // let b = prompt("Please input number:");
+
+   // switch(true) {
+      //  case isFinite(b) && b % 1 === 0:
+      //          console.log("number and integer");
+      //      break;
+      //   case isNaN(b):
+      //         console.log("not a number");
+      //      break;
+
+      //  case b % 1 !== 0:
+      //          console.log("not integer");
+      //      break;
+        
+      //  default:
+      //          console.log("no data");
+ //   }
+
+
+
+
+ 
