ea11508ba3fc076afb462334ed16edfd25cbacce
Katte18 <buleett@gmail.com>
2022-06-19 15:08:17 +0300

    Add files via upload

---
+++ 10task/main.js
@@ -0,0 +1,30 @@
+const sounds = [
+  "fire",
+  "wind",
+  "water",
+  "||"
+];
+
+  sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+      stopSongs();
+      document.getElementById(sound).play();
+    });
+
+
+    document.getElementById("buttons").appendChild(btn);
+  });
+
+  function stopSongs() {
+    sounds.forEach((sound) => {
+      const song = document.getElementById(sound);
+
+      song.pause();
+      song.currentTime = 0;
+    });
+  }
