97f1aabe3d90cdf32d81f65fff016eb9a1fcbc05
Katte18 <buleett@gmail.com>
2022-06-25 22:51:42 +0300

    Update main.js

--- 09task/main.js
+++ 09task/main.js
@@ -17,7 +17,7 @@ function getRandomColor() {
   let color = "#"
 
   for (let i = 0; i < 6; i++) {
-  color += letters[Math.floor(Math.random() * 16)];
+  color += letters[Math.floor(Math.random() * 17)];
   }
  return color;
 }
